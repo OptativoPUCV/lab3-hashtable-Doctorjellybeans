fb10ac366ca0f7dcecb36fe66a360d17d87ec314
Replit user <>
2023-09-06 21:37:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -84,7 +84,7 @@ void enlarge(HashMap * map) {
 
   for (int i = 0; i < map->capacity; i++){
     if (old_buckets[i] != NULL && old_buckets[i]->key != NULL){
-      long indice = hash(old_buckets[i]->key, nuevaCapacidad);
+      //long indice = hash(old_buckets[i]->key, nuevaCapacidad);
       insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
     }
   }
